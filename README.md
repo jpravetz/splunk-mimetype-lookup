8a965803ab2ad1e493d46ef785ba400976a6bbb5
James Pravetz <jpravetz@epdoc.com>
2014-05-15 15:57:31 -0700

    Added URL -> hostname lookup

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-Splunk external lookup script
+Splunk external lookup scripts
 =============================
 
-External lookup script to map mime types to a more generic file type.
+External lookup script to map:
+* mime types to a more generic file type.
+* URLs to hostnames (eg. to 'box.com')
 A python script is used instead of a csv file so that regex can be used.
 
 Installation
@@ -9,7 +11,7 @@ Installation
 
 Refer to [Splunk documentation](http://docs.splunk.com/Documentation/Splunk/6.0.2/Knowledge/Addfieldsfromexternaldatasources).
 
-Copy the file mimetype_lookup.py (gdrive admin console) to $SPLUNK_HOME/etc/searchscripts
+Copy the files mimetype_lookup.py and/or host_lookup.py to $SPLUNK_HOME/etc/searchscripts
 
 Add the following lines to the existing (or new) file $SPLUNK_HOME/etc/system/local/transforms.conf
 
@@ -21,6 +23,15 @@ fields_list = mimeType,fileType
 # filename = mimetypes.csv
 max_matches = 1
 min_matches = 0
+
+[HostUrlLookup]
+external_cmd =host_lookup.py url host
+external_type = python
+fields_list = url,host
+# filename = hostUrl.csv
+max_matches = 1
+min_matches = 0
+
 ```
 
 Add the following lines to the existing (or new) file $SPLUNK_HOME/etc/system/local/props.conf
@@ -28,9 +39,14 @@ Add the following lines to the existing (or new) file $SPLUNK_HOME/etc/system/lo
 ```
 [mimetype_lookup]
 lookup_mimetype = MimeTypeLookup mimeType OUTPUT fileType
+
+[=host_lookup]
+lookup_host = HostUrlLookup url OUTPUT host
 ```
 
-Use this syntax in searches: ```lookup MimeTypeLookup mimeType```
+Use this mimetype lookup syntax in searches: ```lookup MimeTypeLookup mimeType```
+Use this hostname lookup syntax in searches: ```lookup HostUrlLookup url```
+
 
 
 
---
+++ host_lookup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import sys
+import csv
+import urlparse
+
+""" An adapter that takes CSV as input, performs a lookup to map URLS
+    to service names, then returns the CSV results
+
+    Bidrectional mapping is always required when using an external lookup as an
+    'automatic' lookup: one configured to be used without explicit reference in
+    a search.
+"""
+
+
+# Given a url, find a service name
+def lookup(url):
+    try:
+        hostname = mapHostnameUrl(url)
+        return hostname
+    except:
+        return url
+
+# Given an filetype, return it's own value
+def rlookup(service):
+    return service
+
+def mapHostnameUrl(url):
+    result = url
+    urlparts = urlparse.urlparse(url)
+    if urlparts
+        result = urlparts.netloc
+        hparts = urlparts.netloc.split('.')
+        if hparts 
+            result = ".".join(len(hparts[-2]) < 4 and hparts[-3:] or hparts[-2:])
+            # At this point, if we want to combine hostnames (eg. box.com, box12.com) we can do so
+            if result == 'force.com'
+                result = 'salesforce.com'
+            if result == 'box.net'
+                result = 'box.com'
+    return result
+
+def main():
+    if len(sys.argv) != 3:
+        print "Usage: python host_lookup.py [url field] [hostname field]"
+        sys.exit(1)
+
+    hostfield = sys.argv[1]
+    urlfield = sys.argv[2]
+
+    infile = sys.stdin
+    outfile = sys.stdout
+
+    r = csv.DictReader(infile)
+    header = r.fieldnames
+
+    w = csv.DictWriter(outfile, fieldnames=r.fieldnames)
+    w.writeheader()
+
+    for result in r:
+        # Perform the lookup or reverse lookup if necessary
+        if result[hostfield] and result[urlfield]:
+            # both fields were provided, just pass it along
+            w.writerow(result)
+
+        elif result[hostfield]:
+            # only mimetype was provided, add filetype
+            result[urlfield] = lookup(result[hostfield])
+            if result[urlfield]:
+                w.writerow(result)
+
+        elif result[urlfield]:
+            # only filetype was provided, add mimetype
+            result[hostfield] = rlookup(result[urlfield])
+            if result[hostfield]:
+                w.writerow(result)
+
+main()
